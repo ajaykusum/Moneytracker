787da51240f0001276a7a97501850ed46db76ed1
Ajay Kusum <103605834+ajaykusum@users.noreply.github.com>
2023-08-21 19:21:21 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -38,5 +38,5 @@ res.json(transaction);
 
   const port = 4000;
   app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
+    console.log(`Server is running on http://localhost:${port}`);  /* template litrals- combines expression and variable*/
   }); 
